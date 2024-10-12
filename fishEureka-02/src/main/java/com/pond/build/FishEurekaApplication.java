7aa1d3c63c47d8e4448086280080c2ff3d37786d
“hollow288” <571339274@qq.com>
2024-10-12 18:05:46 +0800

    commit

--- fishEureka-02/src/main/java/com/pond/build/FishEurekaApplication.java
+++ fishEureka-02/src/main/java/com/pond/build/FishEurekaApplication.java
@@ -1,5 +1,6 @@
 package com.pond.build;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
