69084492305d044e200d5339f6b1ee8ba5f805c0
“hollow288” <571339274@qq.com>
2024-10-12 18:05:41 +0800

    commit

---
+++ fishBaseService/src/main/java/com/pond/build/config/ConfigChangeListener.java
@@ -0,0 +1,14 @@
+package com.pond.build.config;
+
+import com.ctrip.framework.apollo.model.ConfigChangeEvent;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfigChangeListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConfigChangeListener {
+
+    @ApolloConfigChangeListener
+    public void onChange(ConfigChangeEvent changeEvent) {
+        System.out.println("Detected changes: " + changeEvent.changedKeys());
+    }
+}
--- fishBaseService/src/main/java/com/pond/build/controller/TestController.java
+++ fishBaseService/src/main/java/com/pond/build/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.pond.build.controller;
 
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigService;
 import com.pond.build.remote.TestRemote;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,15 @@ public class TestController {
         String returnMsg = testRemote.hello(name);
         System.out.println(returnMsg);
         return returnMsg;
+    }
+
+
+    @GetMapping("/say")
+    public String index2() {
+        Config config = ConfigService.getAppConfig(); // 获取默认 application 命名空间
+        String someKey = config.getProperty("configValue", "default_value");
+        System.out.println("Config Value: " + someKey);
+        return someKey;
     }
 
 
