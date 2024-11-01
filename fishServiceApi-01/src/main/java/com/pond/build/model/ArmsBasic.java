4f795d4b79b41608d38636a8a644f16850d16eea
“hollow288” <571339274@qq.com>
2024-11-01 17:59:26 +0800

    version

---
+++ fishServiceApi-01/src/main/java/com/pond/build/model/ArmsBasic.java
@@ -0,0 +1,75 @@
+package com.pond.build.model;
+
+import java.math.BigDecimal;
+
+public class ArmsBasic {
+
+    /**
+     * 武器ID
+     */
+    private String armsId;
+    /**
+     * 武器名称
+     */
+    private String armsName;
+    /**
+     * 武器稀有度
+     */
+    private String armsRarity;
+    /**
+     * 武器轮次
+     */
+    private String armsDate;
+    /**
+     * 武器定位
+     */
+    private String armsType;
+    /**
+     * 武器属性
+     */
+    private String armsAttribute;
+    /**
+     * 武器破防
+     */
+    private BigDecimal armsOverwhelmed;
+    /**
+     * 武器充能
+     */
+    private BigDecimal armsChargingEnergy;
+    /**
+     * 武器攻击力-初始
+     */
+    private BigDecimal armsAggressivityStart;
+    /**
+     * 武器血量-初始
+     */
+    private BigDecimal armsBloodVolumeStart;
+    /**
+     * 武器全抗-初始
+     */
+    private BigDecimal armsDefenseCapabilityStart;
+    /**
+     * 武器暴击-初始
+     */
+    private BigDecimal armsCriticalStrikeStart;
+    /**
+     * 武器攻击-满级
+     */
+    private BigDecimal armsAggressivityEnd;
+    /**
+     * 武器血量-满级
+     */
+    private BigDecimal armsBloodVolumeEnd;
+    /**
+     * 武器全抗-满级
+     */
+    private BigDecimal armsDefenseCapabilityEnd;
+    /**
+     * 武器暴击-满级
+     */
+    private BigDecimal armsCriticalStrikeEnd;
+
+
+
+
+}
